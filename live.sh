d04c6296003eec55bf87f3de952ec553f5c805c3
yazan shehab <43929918+yazanshehab@users.noreply.github.com>
2021-06-14 12:58:30 +0300

    Update live.sh

--- live.sh
+++ live.sh
@@ -3,8 +3,8 @@
 echo "Content-type: text/html"
 echo
 
-/home/yeezy/program/ffmpeg/ffmpeg\
- -stream_loop -1 -re  -i /home/yeezy/bun.mp4 \
+/"ffmpeg location"/ffmpeg\
+ -stream_loop -1 -re  -i /"your file location" \
  -c:v libx264 -ldash 1 -streaming 1 -frag_type every_frame -tune zerolatency\
  -use_timeline 1\
  -use_template 1\
