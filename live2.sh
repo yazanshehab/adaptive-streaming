67a00d1a2eb983eed3dd4bd7edf0ce3de902c645
yazan shehab <43929918+yazanshehab@users.noreply.github.com>
2021-06-14 12:58:45 +0300

    Delete live2.sh

--- live2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/home/gorkem/Documents/ffmpeg/ffmpeg\
- -flags2 +export_mvs -stream_loop -1 -re  -i /home/gorkem/Documents/videos/bun.mp4 \
- -c:v libx264 -ldash 1 -streaming 1 -frag_type every_frame -tune zerolatency\
- -use_timeline 1\
- -use_template 1\
- -seg_duration 4\
- -f dash /home/gorkem/Documents/output/vidmanf.mpd
